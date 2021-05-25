36e55d60fcfd8f5401f60b19cfddff9375f69977
Elisa Kuntner <elisa.kuntner@student.uibk.ac.at>
2021-05-25 13:03:06 +0200

    idee füricons

--- main.js
+++ main.js
@@ -14,6 +14,15 @@ L.marker([46.7599, 10.2104]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
 
+
+
+//Icon für Vorteile (ist ein Plus) (https://fontawesome.com/icons/plus-circle?style=regular)  <i class="far fa-plus-square"></i>
+//Icon für Nachteile https://fontawesome.com/icons/minus-square?style=regular <i class="far fa-minus-square"></i>
+//Icon für Entwicklungsbedarf https://fontawesome.com/icons/thermometer-quarter?style=solid <i class="fas fa-thermometer-quarter"></i>
+//Icon für gut Entwickelt https://fontawesome.com/icons/thermometer-three-quarters?style=solid <i class="fas fa-thermometer-three-quarters"></i>
+
+
+
 // hash: zeigt in der URL leiste die Koordinaten und den #zoom des kartenausschnittes an 
 var hash = new L.Hash(map); //Var hash steht hier nur weil man eine neue Var erstellt, falls man sie später noch mal braucht. new  --> nach L muss großbuchstabe sein.
 
