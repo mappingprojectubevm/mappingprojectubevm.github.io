80ff369b5acefeff02b1902f19621361d2ba4f29
Elisa Kuntner <elisa.kuntner@student.uibk.ac.at>
2021-05-27 12:20:22 +0200

    bsp gelöscht

--- main.js
+++ main.js
@@ -43,18 +43,6 @@ for (let entry of COMMENTS){ //muss noch umgebaut werden und marker entsprechend
       <h5> ${entry.ort}:</h5>
       <p class="popup">"${entry.txt}"></p> 
       `).addTo(map);
-}
-
-//BSP
-var myIcon = L.divIcon({className: 'my-div-icon'});
-// you can set .my-div-icon styles in CSS
-
-L.marker([50.505, 30.57], {icon: myIcon}).addTo(map);
-
-
-
-
-
 
 
 
