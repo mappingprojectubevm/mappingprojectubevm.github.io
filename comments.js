d9082834c895ea7e15c87237ea77de5e22a16619
Elisa Kuntner <elisa.kuntner@student.uibk.ac.at>
2021-05-27 10:34:18 +0200

    commnts liste vervollständigt

--- comments.js
+++ comments.js
@@ -16,8 +16,8 @@ const COMMENTS = [{
     nr: 3,
     ort: "Gebiet Valsot", //unsre kartengrundlage zeigt den ort valsot nicht, das ist ein bisschen blöd, finde aber keine andre
     txt: "Dort wo noch kein BR ist könnte noch eine Entwicklung stattfinden",
-    lat: 46.8736,
-    lng: 10.4431,
+    lat: 46.8543,
+    lng: 10.4207,
     id:"Entwicklungsbedarf", 
 },   {
     nr: 4,
@@ -61,7 +61,69 @@ const COMMENTS = [{
     lat: 46.7599,
     lng: 10.2014,
     id:"Vorteil", 
+}, {
+    nr: 10,
+    ort: "Schweizerischer Nationalpark",
+    txt: "Der Schweizericshe Nationalpark ist asl Trennung und zugleich auch als Verbindung zwischen den Tälern zu sehen. Damit stellt er einen Vorteil und zugleich auch einen Nachteil dar.",
+    lat: 46.64842, 
+    lng: 10.26607,
+    id:"keineZuordnung", //Vorteil und nachteil zugleich -> entweder gelb machen oder zwei kästchen  
+}, {
+    nr: 11,
+    ort: "Kernzone",
+    txt: "Der Schweizerische Nationalpark als Kernzone ist hervorragend aufgestellt und etabliert",
+    lat: 46.6739,
+    lng: 10.2369,
+    id:"Entwickelt",
+}, {
+    nr: 12,
+    ort: "Schigebiet Val Müstair",
+    txt: "Naturschutzorganisationen verhindern eine Erweiterung und Ausbau des Schigebietes",
+    lat: 46.6464,
+    lng: 10.3302,
+    id:"Nachteil", 
+}, {
+    nr: 13,
+    ort: "Schigebiet Val Müstair",
+    txt: "Durch einen guten Austausch zwischen den Naturschutzorganisationen und dem Schigebiet könnten Vorteile für beide entstehen.",
+    lat: 46.645,
+    lng: 10.3359,
+    id:"Vorteil", 
+}, {
+    nr: 14,
+    ort: "Außerhalb des bestehenden Siedlungsraums",
+    txt: "Einschränkungen beim Bau und Ausbau von touristischen Infrastrukturen außerhalb des Siedlugnsraumes.",
+    lat: 46.6006,
+    lng: 10.3276,
+    id:"Nachteil", 
+},  {
+    nr: 15,
+    ort: "Val Müstair",
+    txt: "Durch das Biosphärenreservat hat das Münstertal wirtschaftliche Vorteile und ist wirtschafltich gut entwickelt.",
+    lat: 46.6120,
+    lng: 10.3937,
+    id:"Entwickelt", 
+}, {
+    nr: 15,
+    ort: "Entwicklungszone",
+    txt: "Das Biosphärenreservat bringt positive Effekte für das Gebiet der Entwicklungszone. Das Tal und insbesondere der Siedlungsraum Val Müstair können vom Biosphärenreservat profitieren",
+    lat: 46.6164,
+    lng: 10.4043,
+    id:"Vorteil", 
+},  {
+    nr: 16,
+    ort: "Entwicklungszone",
+    txt: "Die Entwicklugnszone des BIosphärenreservates ist gut entwickelt.",
+    lat: 46.6164,
+    lng: 10.4077,
+    id:"Entwickelt", 
+}, {
+    nr: 17,
+    ort: "gesamtes existierendes Biosphärenparkgebiet",
+    txt: "Das gesamte bisherige Gebiet wird als gut entwickelt angesehen, wobei keine Einschränkungen zu finden sind, sondern lediglich Vortiele im Gebiet zu erreichen sind. Besonders die aktuelle Größe des Gebietes ist vortielhaft, da in dieser Größenordung KOmmunikaton gut möglich ist. Auf der anderen Seite wird gesagt, dass die räumliche Trennung der zwei Täler sich auch in Schwierigkeiten der Kommunikation niederschlägt. Hier würde noch Entwicklugnsdarf herrschen.",
+    lat: 46.6721,
+    lng: 10.2619,
+    id:"keine Zuordnung",  //Könnte man auch einfach in den text unter die Karte geben. jedenfalls POpup anders formatieren, oder doch mehrere Popups machen.
 },
 
-//es fehlen momentan die punkte ab dem brauen im unteren teil des bildes
 ]
