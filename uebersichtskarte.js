b954932f5954b820ca23d86ae64162e4cf393cb3
Elisa Kuntner <elisa.kuntner@student.uibk.ac.at>
2021-05-31 15:20:37 +0200

    commi

--- uebersichtskarte.js
+++ uebersichtskarte.js
@@ -40,94 +40,94 @@ for (let config of ZONE) {
   fetch(config.data)
     .then(response => response.json()) //innere runde klammer: Funktionsaufruf, damit es gestartet / ausgeführt wird ! 
     .then(geojsonData => {
-        //console.log("Data: ", geojsonData);
-        if (config.title == "Zonierung des UBEVM") {
-            drawGeometry(geojsonData);
-          }
-        })
-    };
-  //Zonierung "zeichnen" + hinzufügen
-  let drawGeometry = (geojsonData) => {
-    //console.log("Geometry", geojsonData);
-    L.geoJson(geojsonData, {
-      style: (feature) => {
-        return { //Farben noch anpassen
-          color: "darkgreen",
-          fillColor: "green",
-          fillOpacity: 0.3
-        }
-      }, //Popups einbinden und beschriften, damit man auf die Zone klicken kann, wenn wir das wollen
-      onEachFeature: (features, layer) => {
-        layer.bindPopup(`<strong>Zonierung des UBEVM</strong>
+      //console.log("Data: ", geojsonData);
+      if (config.title == "Zonierung des UBEVM") {
+        drawGeometry(geojsonData);
+      }
+    });
+};
+//Zonierung "zeichnen" + hinzufügen
+let drawGeometry = (geojsonData) => {
+  //console.log("Geometry", geojsonData);
+  L.geoJson(geojsonData, {
+    style: (feature) => {
+      return { //Farben noch anpassen
+        color: "darkgreen",
+        fillColor: "green",
+        fillOpacity: 0.3
+      }
+    }, //Popups einbinden und beschriften, damit man auf die Zone klicken kann, wenn wir das wollen
+    onEachFeature: (features, layer) => {
+      layer.bindPopup(`<strong>Zonierung des UBEVM</strong>
       <hr>
       ${features.properties.Zone || ""}<br>
       Größe: ${features.properties.Area_ha|| ""} ha
       `);
-      },
-    }).addTo(overlays.geometry)
-  }
+    },
+  }).addTo(overlays.geometry)
+}
 
 
 
 
 
 
-  // Plugin hash
-  L.hash(map);
+// Plugin hash
+L.hash(map);
 
-  // Plugin Minimap
-  var miniMap = new L.Control.MiniMap(L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"), {
-    toggleDisplay: true, //minimap ein und ausklappbar
-    minimized: false //fangt im eingeklappten zustand an. diese einstellungen kann man alle in der leaflet/github davon nachlesen
-  }).addTo(map);
+// Plugin Minimap
+var miniMap = new L.Control.MiniMap(L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"), {
+  toggleDisplay: true, //minimap ein und ausklappbar
+  minimized: false //fangt im eingeklappten zustand an. diese einstellungen kann man alle in der leaflet/github davon nachlesen
+}).addTo(map);
 
 //Maßstab siehe: https://leafletjs.com/reference-1.7.1.html#control-scale
 L.control.scale({
-  imperial: false
+  imperial: false //löscht meilen raus
 }).addTo(map)
 
 
-  /* DROPDOWN FÜR Quellenangaben
-  When the user clicks on the button, 
-  toggle between hiding and showing the dropdown content */
-  function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
+/* DROPDOWN FÜR Quellenangaben
+When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function (e) {
-    if (!e.target.matches('.dropbtn')) {
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (e) {
+  if (!e.target.matches('.dropbtn')) {
 
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      for (var d = 0; d < dropdowns.length; d++) {
-        var openDropdown = dropdowns[d];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var d = 0; d < dropdowns.length; d++) {
+      var openDropdown = dropdowns[d];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
+}
 
 
-  /* icons einfügen 
+/* icons einfügen 
 
 
 
-  let drawComment = (geojsonData) => {
-    L.geoJson(geojsonData, {
-        onEachFeature: (feature, layer) => {
-            layer.bindPopup(`<strong>${feature.properties.NAME}</strong>
-            <hr>
-            Comment: ${feature.properties.NAME}`)
-        },
-        pointToLayer: (geoJsonPoint, latlng) => {
-            return L.marker(latlng, {
-                icon: L.icon({
-                    iconUrl: 'icons/comment-map-icon.png',
-                    iconSize: [38, 38]
-                })
-            })
-        },
-        attribution: '<a href= "https://"> Stadt Wien</a>,<a href= "">Map Icons Collection<a/>'
-    }).addTo(overlays.sightSeeing);
-  }*/
+let drawComment = (geojsonData) => {
+  L.geoJson(geojsonData, {
+      onEachFeature: (feature, layer) => {
+          layer.bindPopup(`<strong>${feature.properties.NAME}</strong>
+          <hr>
+          Comment: ${feature.properties.NAME}`)
+      },
+      pointToLayer: (geoJsonPoint, latlng) => {
+          return L.marker(latlng, {
+              icon: L.icon({
+                  iconUrl: 'icons/comment-map-icon.png',
+                  iconSize: [38, 38]
+              })
+          })
+      },
+      attribution: '<a href= "https://"> Stadt Wien</a>,<a href= "">Map Icons Collection<a/>'
+  }).addTo(overlays.sightSeeing);
+}*/
